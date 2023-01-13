d88f0e01fa94cf94c5b2bb3fe9d9f2c4554f7ad3
Madison Evans <m.corbinevans@gmail.com>
2023-01-12 17:34:44 -0700

    test passed

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+function manipulate() {
+	let p = document.querySelector("#text");
+	p.textContent = "This is really cool!";
+}
+document.addEventListener("DOMContentLoaded", manipulate);
